69d09ee8e4fb696144925943af0f9c2ade598c12
Nick Thomas <nichoth@gmail.com>
2017-10-04 20:56:33 -0700

    first commit

---
+++ example/index.js
@@ -0,0 +1,38 @@
+import { h, render } from 'preact'
+var xtend = require('xtend')
+var S = require('pull-stream')
+var scan = require('pull-scan')
+var cat = require('pull-cat')
+var ViewStream = require('../')
+
+var View = ViewStream(['foo', 'bar'], function myView (props) {
+    console.log('render', props)
+    return <div>
+        hello {props.hurray}
+        <br />
+        <button onClick={props.events.foo}>foo click</button>
+    </div>
+})
+
+render(<View.view />, document.body)
+
+var strings = [ 'ham', 'string', 'world' ]
+var initState = { hurray: 'hurray' }
+
+S(
+    cat([
+        S.once(initState),
+        S(
+            View.sources.foo(),
+            scan((prev, n) => prev === 2 ? 0 : prev + 1, 0),
+            S.map(n => strings[n]),
+            scan(function (state, string) {
+                return xtend(state, { hurray: string })
+            }, initState),
+         )
+    ]),
+    View.sink
+)
+
+
+
---
+++ index.js
@@ -0,0 +1,77 @@
+import { h, Component } from 'preact'
+var xtend = require('xtend')
+var S = require('pull-stream/pull')
+var Drain = require('pull-stream/sinks/drain')
+var Notify = require('pull-notify')
+var Abortable = require('pull-abortable')
+var mux = require('pull-stream-util/mux')
+
+function PreactStream (evNames, view) {
+    var sources = evNames.reduce(function (acc, name) {
+        var notify = Notify()
+        acc[name] = notify
+        return acc
+    }, {})
+
+    var state = Notify()
+    var abortable = Abortable()
+
+    class ViewStream extends Component {
+
+        componentDidMount() {
+            var self = this
+            S(
+                state.listen(),
+                abortable,
+                Drain(function onUpdate (state) {
+                    self.setState(state)
+                }, function onEnd (err) {
+                    if (err) throw err
+                })
+            )
+        }
+
+        componentWillUnmount() {
+            abortable.abort()
+        }
+
+        render(props, state) {
+            var emitters = Object.keys(sources).reduce(function (acc, k) {
+                acc[k] = function (data) {
+                    sources[k](data)
+                }
+                return acc
+            }, {})
+
+            return h(
+                view,
+                xtend({ events: emitters }, props, state),
+                props.children
+            )
+        }
+    }
+
+    function Sources () {
+        return mux(Object.keys(sources).reduce(function (acc, k) {
+            acc[k] = sources[k].listen()
+            return acc
+        }, {}))
+    }
+
+    Object.keys(sources).forEach(function (k) {
+        Sources[k] = sources[k].listen
+    }, {})
+
+    return {
+        sources: Sources,
+        sink: Drain(function onEvent (ev) {
+            state(ev)
+        }, function onEnd (err) {
+            if (err) throw err
+        }),
+        view: ViewStream
+    }
+}
+
+module.exports = PreactStream
+
